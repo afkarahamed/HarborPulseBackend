b03245f0687fdd2afc418eb4a635babf920f1a70
afkarahamed <afkar.net11@gmail.com>
2025-09-14 21:44:05 +0530

    implemented registering and logging in logic

---
+++ controllers/auth.controller.js
@@ -0,0 +1,87 @@
+const {pool} = require("../database/db");
+const bcrypt = require("bcryptjs");
+
+
+exports.registerUser = async(req, res) =>{
+    const {username, password} = req.body;
+
+    if(!username || !password){
+        const err = new Error("Username and Password are required");
+        err.status = 400;
+        throw err;
+    }
+
+    const [existing] = await pool.query(`
+        SELECT user_id 
+        FROM users 
+        WHERE username= ?;
+        `, [username]);
+
+    if(existing.length){
+        const err = new Error("Username already exists");
+        err.status = 400;
+        throw err;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [result] = await pool.query(`
+        INSERT INTO users
+        (username, password, role)
+        VALUES (?,?,"user");
+        `, [username, hashedPassword]);
+    
+    const insertedId = result.insertId;
+
+    if(!insertedId){
+        const err = new Error("Registration failed");
+        err.status = 500;
+        throw err;
+    }
+
+    res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+        userId: insertedId
+    });
+}
+
+exports.loginUser = async (req, res) => {
+    const {username, password} = req.body;
+
+    if(!username || !password){
+        const err = new Error ("Invalid Credentials");
+        err.status = 400;
+        throw err;
+    }
+
+    const [rows] = await pool.query(
+        "SELECT * FROM users WHERE username = ?",
+        [username]
+    );
+    if (!rows.length) {
+        const err = new Error("Invalid credentials");
+        err.status = 401;
+        throw err;
+    }
+
+    const user = rows[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        const err = new Error("Invalid credentials");
+        err.status = 401;
+        throw err;
+    }
+
+    const token = `dummy-token-${user.user_id}`;
+
+    res.json({
+        success: true,
+        token,
+        user: {
+        user_id: user.user_id,
+        username: user.username,
+        role: user.role
+        }
+    });
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,11 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("../middleware/asyncHandler");
 
+const {registerUser, loginUser} = require ("../controllers/auth.controller");
 
-router.post("/login", asyncHandler(async (req, res) =>{
-    res.locals.response = { success:true};
-    const {response} = res.locals;
-    const finalResponse = {...response, ...req.body};
-    res.json(finalResponse);
-}));
+router.post("/register", asyncHandler(registerUser));
+
+router.post("/login", asyncHandler(loginUser));
 
 router.post("/logout", asyncHandler(async (req, res) =>{
     res.send(req.body);
