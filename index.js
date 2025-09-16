588cc5bada193a7c6d69e75f32fda2214c2d1034
afkarahamed <afkar.net11@gmail.com>
2025-09-16 16:26:50 +0530

    added  handling logs with winston

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const config = require("config");
+const winston = require("winston/lib/winston/config");
 
 
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/dbStartup")();
 
@@ -12,5 +14,5 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || config.get("app.port");
 app.listen(port, () => {
-    console.log("Listening on port " + port);
+    winston.info("Listening on port " + port);
 });
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
-
+const winston = require("winston");
 function errorHandler(err, req, res, next) {
-    console.error(err.stack);
+    winston.error(err.stack);
     res.status(err.status || 500).json({
         success: false, 
         error:{
