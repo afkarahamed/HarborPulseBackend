18c836447ac5bd722417daa4106b087e68c31c3c
afkarahamed <afkar.net11@gmail.com>
2025-09-17 23:39:05 +0530

    added users route

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,8 +1,8 @@
-const {pool} = require("../config/db");
+const {pool} = require("../database/db");
 
 
 exports.getUsers = async (req, res) =>{
-    const [rows] = pool.query("SELECT user_id, username, role FROM users");
+    const [rows] = await pool.query(`SELECT user_id, username, role FROM users`);
 
     if(!rows.length){
         const err = new Error("No users found");
@@ -14,4 +14,33 @@ exports.getUsers = async (req, res) =>{
         success: true, 
         data: rows
     })
+}
+
+exports.postUsersRole = async(req, res) => {
+    const {id} = req.params;
+    const {role} = req.body;
+
+    if(!["user", "admin"].includes(role)){
+        return res.status(400).json({
+            error: "Role must be either 'user' or 'admin'"
+        });
+    }
+
+    const [result] = await pool.query(`
+        UPDATE users 
+        SET role = ? 
+        WHERE user_id = ?`,
+         [role, id]);
+    
+    if(!result.affectedRows ===0){
+        const err = new Error("Update Failed");
+        err.status = 400;
+        throw err;
+    }
+
+    res.json({
+        success: true,
+        message: `User ${id} role updated to ${role}`
+    });
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("../middleware/asyncHandler");
 
-const {getUsers} = require("../controllers/users.controller");
+const {getUsers, postUsersRole} = require("../controllers/users.controller");
 
 const auth = require("../middleware/auth");
 const requireAdmin = require("../middleware/requireAdmin");
 
 router.get("/", auth, requireAdmin, asyncHandler(getUsers));
+
+router.patch("/:id/role", auth, requireAdmin, asyncHandler(postUsersRole));
+
+
+module.exports = router;
